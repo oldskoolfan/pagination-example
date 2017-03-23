2925b26fe20841062fe6a3e69ccb2315bb304f25
Andrew Harris <harris.1305@gmail.com>
2017-03-23 17:20:29 -0400

    mysql hack

--- include/mysql.php
+++ include/mysql.php
@@ -3,4 +3,6 @@ $config = parse_ini_file("{$_SERVER['DOCUMENT_ROOT']}/config.ini");
 $etcPath = $config['etc_directory'];
 include "$etcPath/db-connect.php";
 
+$connection = $con; // hack for homepage
+
 $connection->select_db('babynamesdb');
