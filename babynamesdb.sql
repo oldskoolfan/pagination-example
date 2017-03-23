9b83a695a7b215356eeaeeb997ee8fe2dc6e29cb
Andrew Harris <harris.1305@gmail.com>
2017-03-23 17:13:58 -0400

    fix sql

--- babynamesdb.sql
+++ babynamesdb.sql
@@ -1,6 +1,7 @@
-create database babynamesdb;
+create database if not exists babynamesdb;
 use babynamesdb;
 
+drop table if exists baby_names;
 create table baby_names(
 	id int unsigned not null auto_increment primary key,
     name nvarchar(255)
